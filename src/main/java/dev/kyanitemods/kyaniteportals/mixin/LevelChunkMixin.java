f515e082ac52a56576f769a39f068aa31731c419
Pedro <pedro2007.prrr@gmail.com>
2026-02-22 18:54:58 -0300

    Rename `LevelMixin` to `LevelChunkMixin`

--- src/main/java/dev/kyanitemods/kyaniteportals/mixin/LevelChunkMixin.java
+++ src/main/java/dev/kyanitemods/kyaniteportals/mixin/LevelChunkMixin.java
@@ -2,7 +2,6 @@ package dev.kyanitemods.kyaniteportals.mixin;
 
 import com.llamalad7.mixinextras.sugar.Local;
 import dev.kyanitemods.kyaniteportals.content.registry.PortalTriggers;
-import dev.kyanitemods.kyaniteportals.content.triggers.TriggerResult;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.chunk.LevelChunk;
@@ -12,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LevelChunk.class)
-public abstract class LevelMixin {
+public abstract class LevelChunkMixin {
     @Inject(method = "setBlockState", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;onPlace(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;Z)V", shift = At.Shift.AFTER))
     private void kyanitePortals$blockChangeTrigger(CallbackInfoReturnable<BlockState> cir, @Local(argsOnly = true) BlockPos blockPos, @Local(argsOnly = true) BlockState blockState) {
         PortalTriggers.BLOCK_CHANGE.trigger(((LevelChunk) (Object) this).getLevel(), blockPos, null, blockState);
