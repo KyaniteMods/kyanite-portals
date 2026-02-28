97e047c5f06fb9cf7ce5f3e0f2fdc25a87269213
Pedro <pedro2007.prrr@gmail.com>
2026-02-28 20:44:48 -0300

    Fix server level mixin below 1.21.5

--- src/main/java/dev/kyanitemods/kyaniteportals/mixin/ServerLevelMixin.java
+++ src/main/java/dev/kyanitemods/kyaniteportals/mixin/ServerLevelMixin.java
@@ -3,7 +3,6 @@ package dev.kyanitemods.kyaniteportals.mixin;
 import dev.kyanitemods.kyaniteportals.content.registry.PortalTriggers;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,7 +11,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ServerLevel.class)
 public class ServerLevelMixin {
     @Inject(method = "levelEvent", at = @At("HEAD"))
-    private void kyanitePortals$levelEventTrigger(Entity entity, int type, BlockPos pos, int data, CallbackInfo ci) {
+    //? if <1.21.5 {
+    //private void kyanitePortals$levelEventTrigger(net.minecraft.world.entity.Entity entity, int type, BlockPos pos, int data, CallbackInfo ci) {
+    //? } else
+    private void kyanitePortals$levelEventTrigger(net.minecraft.world.entity.player.Player entity, int type, BlockPos pos, int data, CallbackInfo ci) {
         PortalTriggers.LEVEL_EVENT.trigger((ServerLevel) (Object) this, type, pos, data);
     }
 }
